bdfdf6f303669fa47707253fe60548ff74aeab4d
Tyler Nijmeh <tylernij@gmail.com>
2025-08-20 14:14:11 -0700

    Build web: Wed Aug 20 02:14:11 PM PDT 2025
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3637855892"
+    serviceWorkerVersion: "3800932765"
   }
 });
